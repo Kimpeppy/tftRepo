2475828bbb9b02df080c8614aaba33832c8f3bad
Michael Kim <mike02kim@gmail.com>
2024-02-07 12:59:31 -0500

    set up a script

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mysql from 'mysql2/promise';
+
+const dbConfig = {
+  host: 'localhost',
+  user: 'mkim225',
+  password: '8bg5f4fz',
+  database: 'tftTestDatabase'
+};
+
+// Create a MySQL connection pool
+const pool = mysql.createPool(dbConfig);
 
 const app = express();
 const port = 3000;
